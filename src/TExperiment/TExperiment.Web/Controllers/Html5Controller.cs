8c8bc59862308eff976a5bfd5382c46676636baa
janjunt <janjunt@163.com>
2017-09-19 22:01:21 +0800

    Html5拖放试验

---
+++ src/TExperiment/TExperiment.Web/Controllers/Html5Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TExperiment.Web.Controllers
+{
+    public class Html5Controller : Controller
+    {
+        public ActionResult DragAndDrop()
+        {
+            return View();
+        }
+    }
+}
